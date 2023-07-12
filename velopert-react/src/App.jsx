67dbeb0b727e2c3091144a3bdcd849ea42bf5e02
juseong <jjs7280@gmail.com>
2023-07-12 15:46:35 +0900

    3. 나의 첫번째 리액트 컴포넌트

---
+++ velopert-react/src/App.jsx
@@ -0,0 +1,13 @@
+import Hello from './Components/3/Hello';
+
+function App() {
+  return (
+    <div className='App'>
+      <Hello></Hello>
+      <Hello></Hello>
+      <Hello></Hello>
+    </div>
+  );
+}
+
+export default App;
---
+++ velopert-react/src/Components/3/Hello.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function Hello() {
+  return <div className='text-2xl'>Hello</div>;
+}
