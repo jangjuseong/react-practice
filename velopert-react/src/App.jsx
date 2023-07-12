bd692fe7930a8c113938ad31a5317e1809f5bc57
juseong <jjs7280@gmail.com>
2023-07-12 16:46:52 +0900

    6. 조건부 렌더링

--- velopert-react/src/App.jsx
+++ velopert-react/src/App.jsx
@@ -1,12 +1,12 @@
-import Hello from './Components/5/Hello';
-import Wrapper from './Components/5/Wrapper';
+import Hello from './Components/6/Hello';
+import Wrapper from './Components/6/Wrapper';
 
 function App() {
   return (
     <>
       <Wrapper>
-        <Hello name='주성' color='red'></Hello>
-        <Hello color='pink'></Hello>
+        <Hello name='주성' color='red' isSpecial={true} />
+        <Hello color='pink' />
       </Wrapper>
     </>
   );
