ddda0b5af07531d256f4e0ecdd7b0dfedf796e27
juseong <jjs7280@gmail.com>
2023-07-13 12:43:13 +0900

    12. useRef로 컴포넌트 안의 변수 만들기

---
+++ velopert-react/src/Components/12/UserList.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function User({ user }) {
+  return (
+    <div>
+      <b>{user.username}</b> <span>({user.email})</span>
+    </div>
+  );
+}
+
+export default function UserList({ users }) {
+  return (
+    <div>
+      {users.map(user => (
+        <User user={user} key={user.id} />
+      ))}
+    </div>
+  );
+}
